bccb624cada29b16b4313174a3fbc99254bc11b9
MarwanAmrx <marwan2010417@miuegypt.edu.eg>
2024-01-15 00:33:07 +0200

    //implemted auditable Entity

--- Models/AuditableEntity.cs
+++ Models/AuditableEntity.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-
+//implemted auditable Entity
 namespace CampusCoin.Models
 {
     public class AuditableEntity
