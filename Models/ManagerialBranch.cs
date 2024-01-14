4be378ce18bcaa41b8517269c9ff9e757d2e6455
omarnagy31 <omar2002496@miuegypt.edu.eg>
2024-01-15 01:38:22 +0200

    implemented managerialbranch.cs

--- Models/ManagerialBranch.cs
+++ Models/ManagerialBranch.cs
@@ -8,8 +8,9 @@ namespace CampusCoin.Models
     public class ManagerialBranch
     {
         public int Id { get; set; }
-        public string BranchName { get; set; }
+      
         public string BranchCode { get; set; }
+        public string BranchName { get; set; }
         public string Address { get; set; }
         public ICollection<Admin> Admins { get; set; }
     }
