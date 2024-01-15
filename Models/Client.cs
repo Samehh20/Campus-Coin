15290ab5928d44c83434bc75dbc64381e54bb807
hazem22hassan <hazem2007978@miuegypt.edu.eg>
2024-01-15 17:49:15 +0200

    Implement Client

--- Models/Client.cs
+++ Models/Client.cs
@@ -7,8 +7,8 @@ namespace CampusCoin.Models
 {
     public class Client : AuditableEntity
     {
-        public int Id { get; set; }
         public int? ClientTypeId { get; set; }
+        public int Id { get; set; }
         public virtual ClientType ClientType { get; set; }
         public Guid? UserId { get; set; }
         public virtual User User { get; set; }
